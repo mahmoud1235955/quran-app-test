ccdcc6a9009f51e8729bf3ed45cde178ba484657
Mahmoud Ashour <mahmoud11asd6@gmail.com>
2026-06-12 19:07:35 +0300

    Session 1&2

--- lib/Manager/SurahAudio/cubit/surah_audio_cubit.dart
+++ lib/Manager/SurahAudio/cubit/surah_audio_cubit.dart
@@ -1,8 +1,39 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'surah_audio_state.dart';
 
 class SurahAudioCubit extends Cubit<SurahAudioState> {
-  SurahAudioCubit() : super(SurahAudioInitial());
+  final player = AudioPlayer();
+  bool isPlaying = false;
+
+  SurahAudioCubit() : super(SurahAudioInitial()) {
+    player.onPlayerStateChanged.listen((state) {
+      if (state == PlayerState.playing) {
+        isPlaying = true;
+        emit(PlaySurahAudio());
+      } else if (state == PlayerState.paused || state == PlayerState.stopped) {
+        isPlaying = false;
+        emit(PauseSurahAudio());
+      }
+    });
+  }
+  void playAudio(String url) async {
+    try {
+      await player.play(UrlSource(url));
+      emit(PlaySurahAudio());
+    } catch (error) {
+      emit(SurahAudioError(message: error.toString()));
+    }
+  }
+
+  void pauseAudio() async {
+    try {
+      await player.pause();
+      emit(PauseSurahAudio());
+    } catch (error) {
+      emit(SurahAudioError(message: error.toString()));
+    }
+  }
 }
